aa6a32619f7d5cd1eaab97c7e076416ff312cb2d
Kevin Jolley <jolley427@gmail.comm>
2023-06-27 20:09:24 -0400

    exercise 8.2

---
+++ hash_practice_2.rb
@@ -0,0 +1,13 @@
+languages = {
+  :de => "German",
+  :en => "English",
+  :es => "Spanish",
+}
+dictionary = {
+  :de => { :one => "eins", :two => "zwei", :three => "drei" },
+  :en => { :one => "one", :two => "two", :three => "three" },
+  :es => { :one => "uno", :two => "dos", :three => "tres" },
+}
+
+puts "In #{languages[:de]}, #{dictionary[:de][:one]} means #{dictionary[:en][:one]}, #{dictionary[:de][:two]} means #{dictionary[:en][:two]}, #{dictionary[:de][:three]} means #{dictionary[:en][:three]}."
+puts "In #{languages[:es]}, #{dictionary[:es][:one]} means #{dictionary[:en][:one]}, #{dictionary[:es][:two]} means #{dictionary[:en][:two]}, #{dictionary[:es][:three]} means #{dictionary[:en][:three]}."

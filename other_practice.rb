e55d812a81ba09a8d2d6c353285b9475b1992b81
Kevin Jolley <jolley427@gmail.comm>
2023-06-27 20:42:43 -0400

    exercise 1.3

--- other_practice.rb
+++ other_practice.rb
@@ -2,7 +2,10 @@
 # puts 60 * 24 * 365 * 10
 # puts 60 * 60 * 24 * 365 * 32
 
-puts 3.0 / 2
-puts 3 / 2.0
-puts 4 ** 2.0
-puts 4.1 % 2
+# puts 3.0 / 2
+# puts 3 / 2.0
+# puts 4 ** 2.0
+# puts 4.1 % 2
+
+x = 4
+puts x.even?
